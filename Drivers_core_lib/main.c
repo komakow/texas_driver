699d644566c3429610b8c64d3d532a828efd23a8
komakow <42356216+komakow@users.noreply.github.com>
2018-08-21 21:02:06 +0200

    Add ADC init

--- Drivers_core_lib/main.c
+++ Drivers_core_lib/main.c
@@ -17,6 +17,8 @@ void delay()
 
 GPIOCfg_Type *pin1;
 interrupt void timer0(void);
+interrupt void adc0(void);
+
 volatile uint32_t count;
 
 void configtimer0(void)
@@ -30,17 +32,10 @@ void configtimer0(void)
 
   PieCtrlRegs.PIEIER1.bit.INTx7 = 1;    //hells knows
   CpuTimer0Regs.TCR.bit.TSS = 0;    //start timer
-
-  IER |= M_INT1; //Enable group 1 interrupts
-  EINT;
-  ERTM;
-
-
 }
 
-int main(void)
+void initGpio()
 {
-
   pin1->direction = DIR_Output;
   pin1->pinFunction = MUX_0;
   pin1->type = ODR_NormalOutput;
@@ -52,17 +47,50 @@ int main(void)
   pin1->samplingQualification = QSEL_SYNC;
 
   pinGPIOCfg(pin1);
+
+}
+
+void initADC()
+{
+  EALLOW;
+  CpuSysRegs.PCLKCR13.bit.ADC_A = 1;
+  delay();
+  AdcaRegs.ADCCTL2.bit.PRESCALE = 6;
+  AdcaRegs.ADCSOC0CTL.bit.CHSEL = 0;      //ADC_A0
+  AdcaRegs.ADCSOC0CTL.bit.ACQPS = 24;     //acquiisition prescale
+  AdcaRegs.ADCSOC0CTL.bit.TRIGSEL = 1;    //trig from TIM0
+  AdcaRegs.ADCCTL1.bit.INTPULSEPOS = 1;   //set pulse to late
+  AdcaRegs.ADCINTSEL1N2.bit.INT1SEL = 0; //end of SOC0 will set INT1 flag
+  AdcaRegs.ADCINTSEL1N2.bit.INT1E = 1;   //enable INT1 flag
+  AdcaRegs.ADCCTL1.bit.ADCPWDNZ = 1;      //power up
+  PieCtrlRegs.PIEIER1.bit.INTx1 = 1;      //interrupt from int1
+  AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1; //make sure INT1 flag is cleared
+  delay();
+  EDIS;
+
+}
+
+
+int main(void)
+{
   IER = 0x0000;
   IFR = 0x0000;
 
   InitPieVectTable();
-
   EALLOW;
   PieVectTable.TIMER0_INT = &timer0;
+  PieVectTable.ADCA1_INT = &adc0;
   EDIS;
 
+  initGpio();
   configtimer0();
+  initADC();
 
+  EALLOW;
+  IER = 0x0001;
+  IFR = 0x0000;
+  EINT;
+  EDIS;
 
   while (1)
   {
@@ -75,4 +103,10 @@ interrupt void timer0(void)
   pinGPIOToogle(12);
   PieCtrlRegs.PIEACK.bit.ACK1 = 1;
 
+}
+
+interrupt void adc0(void)
+{
+  PieCtrlRegs.PIEACK.bit.ACK1 = 1;
+  AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1; //make sure INT1 flag is cleared
 }
